9942e97d0a6cafc5dbdbceca54b6e706e7ad0aa9
Shakhzod Namazbaev <shakxzod.namazbaev.9700@gmail.com>
2025-07-24 00:32:11 +0500

    chore: remove api/errors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 export * from './methods';
 export { apiClient } from './client';
 export { ENDPOINTS } from './endpoints';
-export { normalizeError } from './errors';
 export { createAxiosInstance } from './config';
 export { getAuthToken, handleLogout } from './auth';
 export {
